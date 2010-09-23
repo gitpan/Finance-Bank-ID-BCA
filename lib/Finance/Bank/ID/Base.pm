8e096db4129c46913864f648b88061f5c37cef03
Steven Haryanto <stevenharyanto@gmail.com>
2010-09-23 09:56:27 +0000

    Import of SHARYANTO/Finance-Bank-ID-BCA-0.12 from CPAN.
    gitpan-cpan-distribution: Finance-Bank-ID-BCA
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         SHARYANTO/Finance-Bank-ID-BCA-0.12.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Finance/Bank/ID/Base.pm
+++ lib/Finance/Bank/ID/Base.pm
@@ -1,6 +1,6 @@
 package Finance::Bank::ID::Base;
 BEGIN {
-  $Finance::Bank::ID::Base::VERSION = '0.10';
+  $Finance::Bank::ID::Base::VERSION = '0.12';
 }
 # ABSTRACT: Base class for Finance::Bank::ID::BCA etc
 
@@ -243,7 +243,7 @@ Finance::Bank::ID::Base - Base class for Finance::Bank::ID::BCA etc
 
 =head1 VERSION
 
-version 0.10
+version 0.12
 
 =head1 SYNOPSIS
 
--- lib/Finance/BankUtils/ID/Mechanize.pm
+++ lib/Finance/BankUtils/ID/Mechanize.pm
@@ -1,6 +1,6 @@
 package Finance::BankUtils::ID::Mechanize;
 BEGIN {
-  $Finance::BankUtils::ID::Mechanize::VERSION = '0.10';
+  $Finance::BankUtils::ID::Mechanize::VERSION = '0.12';
 }
 # ABSTRACT: A subclass of WWW::Mechanize that does HTTPS certificate verification
 
@@ -48,7 +48,7 @@ Finance::BankUtils::ID::Mechanize - A subclass of WWW::Mechanize that does HTTPS
 
 =head1 VERSION
 
-version 0.10
+version 0.12
 
 =head1 SYNOPSIS
 
